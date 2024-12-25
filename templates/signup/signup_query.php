d774f7eac7621d7140add9db3fab70f28d02d605
Soft Life <jenslinjemz123@gmail.com>
2024-12-25 10:53:23 +0000

    create DB connection and create signup page

---
+++ templates/signup/signup_query.php
@@ -0,0 +1,9 @@
+<?php 
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+
+user_data($username,$password,$email,$number);
+
+
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php 
+
+$username = 'username';
+$password = 'password';
+$email = 'email';
+$number = 'number';
+
+// Print the values for testing
+echo "username: " . $username . "<br>";
+echo "password: " . $password . "<br>";
+echo "email: " . $email . "<br>";
+echo "number: " . $number . "<br>";
