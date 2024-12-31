dbf76c40289c30fb7c1d858dc094fb9ee4337338
Soft Life <jenslinjemz123@gmail.com>
2024-12-31 13:27:21 +0000

    h

--- web/templates/signup/signup_query.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-$number = $_POST['number'];
-
-user_data($username,$password,$email,$number);
-
-
